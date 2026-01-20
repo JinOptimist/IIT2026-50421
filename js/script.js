0e02730116ecf1f351a44f7332d2f46858c54066
JinOptimist <88jin@mail.ru>
2026-01-20 20:20:30 +0300

    add girl

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
+    init();
 
     $('.image').click(function () {
         $(this).addClass('hide-girl');
     });
+
+    function init(){
+        for (let i = 0; i < 6; i++) {
+            createGirl(i + 1);
+        }
+    }
+
+    function createGirl(index){
+        // const div = document.createElement('div')
+        const div = $('<div>');
+        div.addClass('image');
+
+        const img = $('<img>');
+        img.attr('src', `img/girl${index}.jpg`);
+
+        div.append(img);
+        $('.images').append(div);
+    }
 
 });
