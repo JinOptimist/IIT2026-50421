6c3f08c8efa9b9d9bd6788fc62b2291674e86671
JinOptimist <88jin@mail.ru>
2026-01-27 21:04:09 +0300

    Bank account v1 is ready

--- js/bank.js
+++ js/bank.js
@@ -3,16 +3,36 @@ $(document).ready(function () {
         let currentNumber = $('.the-number').text() - 0;
         currentNumber -= 5;
         $('.the-number').text(currentNumber);
-        flyNumber();
+        flyNumber(-5);
     });
 
     $('.add-five-coin').click(function () {
         let currentNumber = $('.the-number').text() - 0;
         currentNumber += 5;
         $('.the-number').text(currentNumber);
+        flyNumber(5);
     });
 
-    function flyNumber(number){
-        $('.fly-number').css('top', -200);
+    function flyNumber(number) {
+        // create elemtn which will fly
+        const span = $('<span>');
+        span.addClass('fly-number');
+        if (number > 0) {
+            span.addClass('positive');
+        } else {
+            span.addClass('negative');
+        }
+        span.text(number);
+        $('.coins').append(span);
+
+        // Change property to move element
+        setTimeout(() => {
+            const leftPosition = number > 0
+                ? 300
+                : -100;
+            span.css('top', -250);
+            span.css('left', leftPosition);
+            span.css('opacity', 0);
+        }, 100)
     }
 });
