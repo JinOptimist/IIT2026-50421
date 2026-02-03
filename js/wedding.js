f2b85042fda740517f4ee08ebb75bc2ee0257036
JinOptimist <88jin@mail.ru>
2026-02-03 21:46:38 +0300

    scroll to wedding

--- js/wedding.js
+++ js/wedding.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-    $('.envelop').click(function () {
-        
+    let couterForGirl = 0;
+    const girlCount = 3;
+    init();
+
+    $('.envelop-bottom,.envelop-up').click(function () {
         const parent = $(this)
             .closest('.envelop-container');
         const envelopUp = parent
@@ -15,9 +18,40 @@ $(document).ready(function () {
             parent.css('overflow', 'visible');
             envelopUp.css('transition', 'none')
             envelopUp.css('z-index', 3);
-            envelopContent.addClass('opened');
+            scrollContent();
         }, 2200)
     });
 
+    $('.envelop-content').click(scrollContent);
 
+    function scrollContent() {
+        const newTop = calcTopForContent();
+        $('.envelop-content').css('top', newTop);
+        couterForGirl++;
+        if (couterForGirl < girlCount) {
+            setTimeout(scrollContent, 1900);
+        }
+    }
+
+    function calcTopForContent() {
+        let top = (-1) * 300; // initial value -300px
+        for (let i = 0; i < couterForGirl; i++) {
+            top -= $(`[girl-id=${i}]`).height();
+        }
+
+        return top;
+    }
+
+    function init() {
+        for (let i = 0; i < girlCount; i++) {
+            const girlImg = $('<img>');
+            const indexStr = i >= 9
+                ? '' + (i + 1)
+                : '0' + (i + 1);
+            girlImg.attr('src', `img/wedding/wedding-girl-${indexStr}.jpg`)
+            girlImg.addClass('girl')
+            girlImg.attr('girl-id', i);
+            $('.envelop-content').append(girlImg);
+        }
+    }
 });
